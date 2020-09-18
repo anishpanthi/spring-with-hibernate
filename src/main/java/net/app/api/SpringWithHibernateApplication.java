4f88150b94827d3b5c4c81e7c73c6ee5e826545a
Anish Panthi <anish.panthi41@gmail.com>
2020-09-18 14:01:24 -0500

    Initial Commit

---
+++ src/main/java/net/app/api/SpringWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package net.app.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithHibernateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringWithHibernateApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/app/api/SpringWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package net.app.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithHibernateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
