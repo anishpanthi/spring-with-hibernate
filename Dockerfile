a907dc1e2c14521bb8e0d824872e7d7be482eea0
Anish Panthi <anishpanthi@Anishs-MBP.lan1>
2020-09-28 22:30:50 -0500

    Updated props

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
+#Add openjdk
 FROM openjdk:11
-
 ADD build/libs/spring-with-hibernate-0.1.jar app.jar
-
 EXPOSE 8080
-
 ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar" ]
--- build-and-push-to-docker.sh
+++
@@ -1,7 +0,0 @@
-./gradew clean build
-docker build -t spring-with-hibernate .
-# docker images
-docker tag <imageID> anishpanthi/springbootapp:0.0.1
-docker push anishpanthi/springbootapp
-docker-compose up -d
-docker ps
