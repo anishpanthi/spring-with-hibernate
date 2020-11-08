855690c8ffab9bb283ac413502eb11e881ce5ab0
Anish Panthi <anish.panthi41@gmail.com>
2020-11-07 22:17:08 -0600

    Refactored main class

--- src/main/java/net/app/api/SpringAndHibernate.java
+++ src/main/java/net/app/api/SpringAndHibernate.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWithHibernateApplication {
+public class SpringAndHibernate {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringWithHibernateApplication.class, args);
+    SpringApplication.run(SpringAndHibernate.class, args);
   }
 }
